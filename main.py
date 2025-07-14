83016e7bbbdf4c9ce7d037ba2b84230258d7fb69
Juan Ignacio Fernandez <jifdesantis90@gmail.com>
2025-07-13 23:33:18 -0300

    restrict fps

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clk = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clk.tick(60)/1000
 
 
 if __name__ == "__main__":
